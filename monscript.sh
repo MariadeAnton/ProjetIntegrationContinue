6e740c0d0d794e314c41e669de36e22a07e96e5e
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-14 19:18:34 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -11,6 +11,7 @@ git config --global user.name "Dorlige"
 
 #clone branch gh-pages
 #git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/  gh-pages > /dev/null
+ echo -e "On fait un clone\n"
 git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git gh-pages > /dev/null
 
 cd gh-pages
@@ -22,15 +23,15 @@ cp  -Rf $HOME/html/faq.html ./index.html
 
  #add, commit and push files
  git add --all .
- echo -e "On fait un commit\n"
+
  
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
-#git push -fq origin gh-pages > /dev/null
+git push -f -q origin gh-pages > /dev/null
  #git push -fq origin master > /dev/null
  
  echo -e "On fait un push sur master\n"
 
- git push --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git  gh-pages> /dev/null
+ #git push --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git  gh-pages> /dev/null
 
 
 
