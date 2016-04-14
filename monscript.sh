d41cbf3d2eb9feddd9ab355159cabedb99072488
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-14 20:01:15 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -12,7 +12,7 @@ git config --global user.name "Dorlige"
 #clone branch gh-pages
 #git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/  gh-pages > /dev/null
  echo -e "On fait un clone\n"
-#git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git gh-pages > /dev/null
+git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git gh-pages > /dev/null
 
 
 #copie de la nouvelle version
