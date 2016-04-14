4f9ce443dceb5d6cc3d42833cdbd1ac0c021b307
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-14 03:04:48 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -7,7 +7,7 @@ cd $HOME
 
 git config --global user.email "saindy.nkadi@yahoo.fr"
 git config --global user.name "Dorlige"
-git config --global push.default simple
+
 
 #clone branch gh-pages
 #git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
