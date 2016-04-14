b5e443219abc0ed0a70861863291267e55580d01
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-14 23:51:26 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -8,23 +8,17 @@ cd $HOME
 git config --global user.email "saindy.nkadi@yahoo.fr"
 git config --global user.name "Dorlige"
 
-#clone branch gh-pages
-#git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/  gh-pages > /dev/null
- echo -e "On fait un clone\n"
 git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git gh-pages > /dev/null
 
 cd gh-pages
  git remote rm origin
-  git remote add origin https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git
+ git remote add origin https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git
 
-#copie de la nouvelle version
 cp  -Rf $HOME/html/faq.html ./index.html
 
- #add, commit and push files
  git add -f index.html
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
-  echo -e "On fait un push sur gh-pages\n"
+ 
   git push -fq origin gh-pages  > /dev/null
- #git push --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git  master > /dev/null
  echo -e "Deploy completed\n"
  #fi
