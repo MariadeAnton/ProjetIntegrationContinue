54c5928ee353346d6d98b0465df1e2a4910f8555
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-14 21:26:16 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -12,9 +12,15 @@ git config --global user.name "Dorlige"
 #clone branch gh-pages
 #git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/  gh-pages > /dev/null
  echo -e "On fait un clone\n"
-git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git doc > /dev/null
+#git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git doc > /dev/null
 
-cd doc
+git clone https//Dorlige:66b54f59bdcc875aa4abda9c181cfda8591da717@github.com/Dorlige/ProjetIntegrationContinue.git --branch=gh-pages gh-pages
+
+
+cd gh-pages
+git remote rm origin
+git remote add origin https//Dorlige:66b54f59bdcc875aa4abda9c181cfda8591da717@github.com/Dorlige/ProjetIntegrationContinue.git
+
 
 #copie de la nouvelle version
 cp  -Rf $HOME/html/faq.html ./index.html
@@ -22,14 +28,14 @@ cp  -Rf $HOME/html/faq.html ./index.html
  #add, commit and push files
  git add -f index.html
 
- git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to doc "
+ git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
   echo -e "On fait un push sur master\n"
-#git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages > /dev/null
  #git push -fq origin doc > /dev/null
  
 
 
- git push --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git  master > /dev/null
+ #git push --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git  master > /dev/null
 
 
 
