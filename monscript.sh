f3ebc1a3bc97952d27f0074b2d46c5bc9731db88
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-15 17:25:19 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -21,6 +21,8 @@ cd gh-pages
  git add -f .
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
  
+ git checkout gh-pages
+ 
   git push -fq origin gh-pages  
   #git push -fq https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git master > /dev/null
  echo -e "Deploy completed\n"
